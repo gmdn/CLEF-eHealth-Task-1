cf1735c34e20ba64737c35751eea7b93af71e30f
Giorgio Maria Di Nunzio <giorgiomaria.dinunzio@gmail.com>
2017-05-12 13:16:04 +0200

    load dictionary corrected.

--- utils/load_dictionary.R
+++ utils/load_dictionary.R
@@ -1,8 +1,7 @@
 # load corrected dictionary 
-american_dictionary <- read.csv2("../data/CLEFeHealth2017Task1_training_EN/raw/dictionary/AmericanDictionary_corrected.csv",
-                                 #american_dictionary <- read.csv2("../data/CLEFeHealth2017Task1_training_EN/raw/dictionary/AmericanDictionary.csv",
+american_dictionary <- read.csv2("../data/CLEFeHealth2017Task1_training_EN/raw/dictionary/AmericanDictionary.csv",
+#american_dictionary <- read.csv2("../data/CLEFeHealth2017Task1_training_EN/raw/dictionary/AmericanDictionary_corrected.csv",
                                  colClasses = c(rep("character", 7),
                                                 rep("numeric", 2), 
-                                                #rep(c("date", "character"), 2),
                                                 rep("character", 8)),
                                  stringsAsFactors = FALSE)
